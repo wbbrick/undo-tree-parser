c3bd1fc12b5c039641f2ce9a8a096ec908cb18db
Bryan Brick <wbbrick@gmail.com>
2016-07-27 02:17:20 -0400

    fix parser to account for true values prepending timestamps

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 var fs = require('fs');
 var parse = require('sexpr-plus').parse;
 
-
-function isNaN(value) {
-    return typeof(value) === 'number' && value != +value;
-}
-
-function processNode( node ){
-	if( node.type === 'list' ) {
-		return node.content.map( processNode );
-	} else {
-		return {
-			'type': node.type,
-			'content': node.content
-		};
-	}
-}
-
-function findTimestamps( memo, it ) {
-	if( it.type === 'list' ) {
-
-		//condition for a timestamp
-		if( it.content.length === 4
-			&& it.content.reduce( ( memo, n ) => memo && !isNaN( parseInt( n.content, 0 ) ), true )
-		  ) {
-			  return memo.concat( [it.content] );
-		  } else {
-			  return memo.concat( it.content.reduce( findTimestamps, [] ) );
-		  }
-	}
-	return memo;
-}
-
-function processData( err, output ) {
+(function (){
 	'use strict';
-	let rawString = output.toString();
-	let rawExpr = rawString.slice( rawString.indexOf( '\n' ) + 1 );
-	let expr = parse( rawExpr );
-	let processedList = expr.map( processNode );
-	let timeStamps = expr.reduce( findTimestamps, [] );
-	console.dir( JSON.stringify( timeStamps ) );
+	function isNaN(value) {
+		return typeof(value) === 'number' && value != +value;
+	}
 
-}
+	function processNode( node ) {
+		if( node.type === 'list' ) {
+			return node.content.map( processNode );
+		} else {
+			return {
+				'type': node.type,
+				'content': node.content
+			};
+		}
+	}
 
-fs.readFile( 'test.undo', processData );
+	function findTimestamps( memo, it ) {
+		if( it.type === 'list' ) {
+
+			//remove true values as sometimes they prepend timestamps and mess up our count
+			let content = it.content.filter( ( item ) => item.content !== 't' );
+
+			//condition for a timestamp
+			let isTimestamp =
+					content.length === 4
+					&& content.reduce( ( memo, n ) => memo && !isNaN( parseInt( n.content, 0 ) ), true );
+
+			if( isTimestamp ) {
+				return memo.concat( [content.map( ( item ) => parseInt( item.content, 0 ) )] );
+			} else {
+				return memo.concat( content.reduce( findTimestamps, [] ) );
+			}
+		}
+		return memo;
+	}
+
+		function processData( err, output ) {
+			let rawString = output.toString();
+			let rawExpr = rawString.slice( rawString.indexOf( '\n' ) + 1 );
+			let expr = parse( rawExpr );
+			let processedList = expr.map( processNode );
+			let timeStamps = expr.reduce( findTimestamps, [] );
+			console.dir( timeStamps );
+
+		}
+
+		fs.readFile( 'test.undo', processData );
+
+	}());
