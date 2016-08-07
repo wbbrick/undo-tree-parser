c6428e56482786c059603933ced6296b37743640
Bryan Brick <wbbrick@gmail.com>
2016-08-07 14:57:38 -0400

    Add an editor type option to the CLI

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import { readFile } from 'fs';
 import { processData } from './parser';
 import yargs from 'yargs';
 
-var args = yargs
-    .usage('$0 <cmd> [args]')
-    .help('help')
-    .argv;
+let args = yargs
+		.usage('$0 [args]')
+		.options({
+			'e': {
+				alias: 'editor',
+				demand: false,
+				default: 'emacs',
+				choices: ['emacs', 'vim'],
+				describe: 'The editor type to parse',
+				type: 'string'
+			}
+		})
+		.help('help')
+		.argv;
 
-readFile( args._[0], ( err, output ) => err ? err : processData( output ) );
+let options = {
+	
+};
+
+readFile( args._[0], ( err, output ) => err ? err : processData( output, options ) );
