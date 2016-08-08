fb1b5fee61fcd7e38c8df186c87bed769763cc0f
Bryan Brick <wbbrick@gmail.com>
2016-08-07 20:08:40 -0400

    Create separate CLI and Node API shims

---
+++ src/cli.js
@@ -0,0 +1,62 @@
+import { readFile } from 'fs';
+import { processData } from './parser';
+import yargs from 'yargs';
+
+let args = yargs
+		.usage('Usage: $0 <filename> [options]')
+		.example(
+			'$0 test.undo -e emacs -f unix -o myTimes.json',
+			'Read timestamps the emacs-formatted persistent undo file "test.undo" and output a ' +
+				'json array of timestamps to myTimes.json'
+		)
+		.options({
+			'e': {
+				alias: 'editor',
+				demand: false,
+				default: 'emacs',
+				choices: ['emacs', 'vim'],
+				describe: 'The editor type to parse',
+				type: 'string'
+			}
+		})
+		.options({
+			'f': {
+				alias: 'format',
+				demand: false,
+				default: 'unix',
+				describe: 'The datetime format (as specified by moment.js)',
+				type: 'string'
+			}
+		})
+		.options({
+			'o': {
+				alias: 'output',
+				demand: false,
+				default: 'process.stdout',
+				describe: 'The output file',
+				type: 'string'
+			}
+		})
+		.alias( 'h', 'help' )
+		.help('help')
+		.argv;
+
+let options = {
+	'editor': args.editor,
+	'format': args.format,
+	'output': args.output
+};
+
+function processFile( err, output ) {
+	if( err ) {
+		throw err;
+	} else {
+		processData( output, options );
+	}
+}
+
+try {
+	readFile( args._[0], processFile );
+} catch ( e ) {
+	throw e;
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
-import { readFile } from 'fs';
 import { processData } from './parser';
-import yargs from 'yargs';
 
-let args = yargs
-		.usage('$0 [args]')
-		.options({
-			'e': {
-				alias: 'editor',
-				demand: false,
-				default: 'emacs',
-				choices: ['emacs', 'vim'],
-				describe: 'The editor type to parse',
-				type: 'string'
-			}
-		})
-		.help('help')
-		.argv;
-
-let options = {
-	
-};
-
-readFile( args._[0], ( err, output ) => err ? err : processData( output, options ) );
+export default function parse( data, { editor='emacs', format='unix' } ) {
+    try {
+        return processData( data, { editor, format } );
+    } catch( e ) {
+        throw e;
+    }
+}
